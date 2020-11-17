b06427233aee5c2d822f23c34c21f87dedaf3fc1
sam <sammybonfanti@gmail.com>
2020-11-17 15:21:53 -0700

    done

---
+++ function declaration vs expressions/script.js
@@ -0,0 +1,12 @@
+function percentageOfWorld1 (population) {
+    return (population / 7900) * 100
+}
+const usa = percentageOfWorld1(300)
+console.log(usa);
+
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100
+}
+const italy = percentageOfWorld2(60)
+console.log(italy);
