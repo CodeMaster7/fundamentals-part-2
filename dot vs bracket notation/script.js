485ac1104dd456235251ecbf3b0cdd3dcb960838
sam <sammybonfanti@gmail.com>
2020-11-18 17:11:19 -0700

    done

---
+++ dot vs bracket notation/script.js
@@ -0,0 +1,9 @@
+const myCountry = {
+    country: 'USA',
+    capital: 'Washington',
+    language: 'English',
+    population: 300,
+    neighbours: ['Canada', 'Mexico']
+}
+
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`);
